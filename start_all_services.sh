80d4a0b2285594adb8208209a08b3995711a4138
Elbios <elbiosgh@gmail.com>
2024-02-09 21:17:33 +0100

    Add --convert to whispercpp

--- start_all_services.sh
+++ start_all_services.sh
@@ -6,7 +6,7 @@ if [ "${SERVICE_OPTION}" = "koboldcpp" ]; then
 fi
 
 # Redirecting stdout and stderr to separate log files for whispercpp
-cd /whispercpp/whisper.cpp && ./server -m models/${WHISPERCPP_MODEL_FILENAME} --host 0.0.0.0 --port 8070 > whispercpp_log.txt 2>&1 &
+cd /whispercpp/whisper.cpp && ./server -m models/${WHISPERCPP_MODEL_FILENAME} --host 0.0.0.0 --port 8070 --convert > whispercpp_log.txt 2>&1 &
 
 # Run XTTS server
 cd /xtts_app
