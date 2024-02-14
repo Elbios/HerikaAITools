65205ac2e91428f0bf7880a447cd920414aaaaa5
Elbios <elbiosgh@gmail.com>
2024-02-14 22:12:57 +0100

    Fix INSTALL.bat and RUN.bat format bugs

--- RUN.bat
+++ RUN.bat
@@ -5,13 +5,11 @@ set /p IncludeTTS=Do you want to run XTTS? (y/n):
 set /p ServiceOption=Do you want to run koboldcpp? (y/n):
 set /p UseQwen=Do you want run Qwen vision model? (y/n):
 
+
 echo HERIKA: Starting Docker daemon...
 wsl -d DwemerAI4Skyrim2 -e sudo nohup dockerd > docker.log 2>&1 &
-if NOT %ERRORLEVEL% == 0 (
-    echo HERIKA: ERROR: Failed to start Docker daemon. Please check the log above for details.
-    pause
-    exit /b %ERRORLEVEL%
-)
+
+echo HERIKA: Waiting for Docker to be ready..
 timeout /t 5 /nobreak > NUL
 
 echo HERIKA: Building Docker image...
@@ -48,7 +46,7 @@ if NOT %ERRORLEVEL% == 0 (
 )
 
 echo HERIKA: Running Docker container...
-wsl -d DwemerAI4Skyrim2 -e bash -c "cd /home/dwemer/HerikaAITools && docker run --gpus all -d %TTSArg2% %ServiceOptionArg2% %QwenArg2% -p 5001:5001 -p 8070:8070 -p 80:80 -p 8007:8007 -v \$(pwd):/home/ubuntu --name herikadocker herikadocker"
+wsl -d DwemerAI4Skyrim2 -e bash -c "cd /home/dwemer/HerikaAITools && docker run --gpus all -d %TTSArg2% %ServiceOptionArg2% %QwenArg2% -p 5001:5001 -p 8070:8070 -p 80:80 -p 8007:8007 -v /home/dwemer/HerikaAITools:/home/ubuntu --name herikadocker herikadocker"
 if NOT %ERRORLEVEL% == 0 (
     echo HERIKA: ERROR: Failed to run Docker container. Please check the log above for details.
     pause
